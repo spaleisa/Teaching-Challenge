3ac0488b972e985f91af99aefed4c7d8451dd59d
Isabella Spaletta <spaleisa@uw.edu>
2015-11-20 10:50:53 -0800

    merging changes

---  js/app.js
+++  js/app.js
@@ -6,18 +6,18 @@ $(function() {
 	// teach.circle = teach.paper.circle(500, 100, 40);
 	// teach.rect = teach.paper.rect(10, 100, 300, 200);
 
-	var paper = Raphael('draw', 0, 0, 1000, 1000);
-	var rect = paper.rect(5, 5, 1000, 1000);
+	var paper = Raphael("container", 1000, 1000);
+	var rect = paper.rect(0, 0, 1000, 1000);
 
 	//var c = paper.circle(400, 250, 20);
 
-	//var d = 50;
 	var grid = function() {
-		var d = 50;
-		for (var i = 1; i <= 9; i++) {
-			var r1 = paper.rect(d*i, d, d, d);
-			for (var j = 1; j <= 9; j++) {
-				var r2 = paper.rect(d*i, d*j, d, d);
+		var position = 50;
+		var height = 50;
+		var width = 50;
+		for (var i = 1; i <= 10; i++) {
+			for (var j = 1; j <= 10; j++) {
+				var r = paper.rect(position*i, position*j, width, height);
 			}
 		}
 	};
